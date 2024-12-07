5d49d9e82a1148e695e998f62c3983d4281b76a1
Carlos Daniel <carlosdanielsantos458@gmail.com>
2024-12-07 13:58:01 -0300

    otimizado u

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expand", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
